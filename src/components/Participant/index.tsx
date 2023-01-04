20588004db8ca16852e0e7c7d4b87ddcc998857d
Pedro Henrique Lopes <pedro.lopes@ligainvest.com.br>
2023-01-04 17:04:01 -0300

    add new project

---
+++ src/components/Participant/index.tsx
@@ -0,0 +1,16 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+import { styles } from "./styles";
+import { IParticipant } from "./types";
+
+export function Participant({ name, onRemove }: IParticipant) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{name}</Text>
+
+      <TouchableOpacity style={styles.button} onPress={onRemove}>
+        <Text style={styles.buttonText}>-</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
